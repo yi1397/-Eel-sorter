206e9935e2f9003ccd953fcd4bfebbf5e1753a44
Jung Yuil <yi1397a@gmail.com>
2020-02-03 15:57:50 +0900

    200203

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 using namespace cv;
 using namespace std;
 
+double calc_dist(Point A, Point B)
+{
+	double dist;
+	int x_dist = A.x - B.x;
+	int y_dist = A.y - B.y;
+	dist = x_dist * x_dist + y_dist * y_dist;
+	return sqrt(dist);
+
+}
+
 void on_trackbar(int, void*)
 {
 
@@ -36,7 +46,6 @@ void detect_eel(Mat& input, Mat& output, int brightness)
 		}
 	}
 	threshhold_img = channels[2];
-	imshow("test", threshhold_img);
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	findContours(threshhold_img, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
@@ -49,20 +58,39 @@ void detect_eel(Mat& input, Mat& output, int brightness)
 		{
 			max_contour = i;
 		}
+		//cout << "i:" << i << endl;
+		//cout << "size:" << contours[i].size() << endl;
 	}
-	
-	for (vector<Point>::size_type i; i < contours[max_contour].size(); i++)
+	//cout << "max_contour:" << max_contour << endl;
+	if (cnt != 0)
 	{
-
+		double min_dist = 10e+10;
+		Point minA;
+		Point minB;
+		//vector<Point> detect_contour;
+		//copy(contours[max_contour].begin(), contours[max_contour].end(), detect_contour.begin());
+		for (int i = 0; i < contours[max_contour].size()/2; i++)
+		{
+			int j = (i + contours[max_contour].size() / 2);
+			int dist = calc_dist(contours[max_contour][i], contours[max_contour][j]);
+			if (dist < min_dist)
+			{
+				min_dist = dist;
+				minA = contours[max_contour][i];
+				minB = contours[max_contour][j];
+			}
+		}
+		line(detect, minA, minB, Scalar(255, 0, 0), 2);
+		putText(detect, to_string(min_dist), Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(0, 0, 255), 2);
+		//cout << min_dist << "px" << endl;
 	}
-
 	drawContours(cam_img, contours, max_contour, Scalar(0, 0, 255), 1, 8, hierarchy, 0, Point());
 	drawContours(detect, contours, max_contour, Scalar(0, 0, 255), 1, 8, hierarchy, 0, Point());
 	//cout << cnt << endl;
-	//cvtColor(detect, detect, COLOR_GRAY2BGR);
-	putText(detect, to_string(sum), Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(0, 0, 255), 2);
-
-	hconcat(cam_img, detect, output);
+	cvtColor(threshhold_img, threshhold_img, COLOR_GRAY2BGR);
+	putText(threshhold_img, to_string(sum), Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(0, 0, 255), 2);
+	hconcat(cam_img, threshhold_img, output);
+	hconcat(output, detect, output);
 }
 
 int main()
